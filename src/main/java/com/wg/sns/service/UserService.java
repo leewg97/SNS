10058ff253c70ad741886c81beb3e487fffdbbc3
leewg97 <leewg97@naver.com>
2022-11-09 13:40:53 +0900

    Feat #1 : Update UserService * Jwt를 활용한 로그인 로직 구현

--- src/main/java/com/wg/sns/service/UserService.java
+++ src/main/java/com/wg/sns/service/UserService.java
@@ -5,36 +5,45 @@ import com.wg.sns.exception.SnsApplicationException;
 import com.wg.sns.model.User;
 import com.wg.sns.model.entity.UserEntity;
 import com.wg.sns.repository.UserEntityRepository;
+import com.wg.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserEntityRepository userEntityRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
 
     public User join(String username, String password) {
-
         userEntityRepository.findByUsername(username).ifPresent(it -> {
             throw new SnsApplicationException(ErrorCode.DUPLICATED_USERNAME, String.format("%s is duplicated", username));
         });
 
         UserEntity userEntity = userEntityRepository.save(UserEntity.of(username, passwordEncoder.encode(password)));
-
         return User.fromEntity(userEntity);
     }
 
     public String login(String username, String password) {
-        UserEntity userEntity = userEntityRepository.findByUsername(username).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USERNAME, ""));
+        UserEntity userEntity = userEntityRepository.findByUsername(username).orElseThrow(
+                () -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not found", username))
+        );
 
-        if (!userEntity.getPassword().equals(password)) {
-            throw new SnsApplicationException(ErrorCode.DUPLICATED_USERNAME, "");
+        if (!passwordEncoder.matches(password, userEntity.getPassword())) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
-        return "LOGIN SUCCESS";
+        return JwtTokenUtils.generateToken(username, secretKey, expiredTimeMs);
     }
 
 
