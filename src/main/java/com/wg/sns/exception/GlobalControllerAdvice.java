90e33895d93d52a0a0d92116bc0a0ff3e486dc2e
leewg97 <leewg97@naver.com>
2022-11-09 13:37:05 +0900

    Feat #1 : Update GlobalControllerAdvice
    * `@ExceptionHandler` 추가

--- src/main/java/com/wg/sns/exception/GlobalControllerAdvice.java
+++ src/main/java/com/wg/sns/exception/GlobalControllerAdvice.java
@@ -2,6 +2,7 @@ package com.wg.sns.exception;
 
 import com.wg.sns.controller.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +15,14 @@ public class GlobalControllerAdvice {
     public ResponseEntity<?> applicationHandler(SnsApplicationException e) {
       log.error("Error occurs {}", e.toString());
         return ResponseEntity.status(e.getErrorCode().getStatus())
-                .body(Response.error((e.getErrorCode()).name()));
+                .body(Response.error(e.getErrorCode().name()));
     }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> applicationHandler(RuntimeException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
+    }
+
 }
