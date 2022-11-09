e0c0c97ba3e49a4273af4281eb73ec332c39b477
leewg97 <leewg97@naver.com>
2022-11-09 13:39:21 +0900

    Feat #1 : Update UserController
    * Modify url
    * Add login api

--- src/main/java/com/wg/sns/controller/UserController.java
+++ src/main/java/com/wg/sns/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.wg.sns.controller;
 
 import com.wg.sns.controller.request.UserJoinRequest;
+import com.wg.sns.controller.request.UserLoginRequest;
 import com.wg.sns.controller.response.Response;
 import com.wg.sns.controller.response.UserJoinResponse;
-import com.wg.sns.model.User;
+import com.wg.sns.controller.response.UserLoginResponse;
 import com.wg.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,15 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/vi/users")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
-        User user = userService.join(request.getUsername(), request.getPassword());
-        return Response.success(UserJoinResponse.fromUser(user));
+        return Response.success(UserJoinResponse.fromUser(userService.join(request.getUsername(), request.getPassword())));
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
+        return Response.success(new UserLoginResponse(userService.login(request.getUsername(), request.getPassword())));
     }
 
 }
