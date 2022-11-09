d535c7042c55648bb67f314049f338cc76e584c5
leewg97 <leewg97@naver.com>
2022-11-09 15:51:58 +0900

    Feat #2 : Update ErrorCode

--- src/main/java/com/wg/sns/exception/ErrorCode.java
+++ src/main/java/com/wg/sns/exception/ErrorCode.java
@@ -11,6 +11,9 @@ public enum ErrorCode {
     DUPLICATED_USERNAME(HttpStatus.CONFLICT, "Username is duplicated"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
+
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 
     private final HttpStatus status;
