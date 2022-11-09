d2134f5b7dcb19acd99084de88c4c61595a7732d
leewg97 <leewg97@naver.com>
2022-11-09 13:36:16 +0900

    Feat #1 : Update ErrorCode
    * ErrorCode 추가

--- src/main/java/com/wg/sns/exception/ErrorCode.java
+++ src/main/java/com/wg/sns/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    DUPLICATED_USERNAME(HttpStatus.CONFLICT, "Username is duplicated");
+    DUPLICATED_USERNAME(HttpStatus.CONFLICT, "Username is duplicated"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 
     private final HttpStatus status;
     private final String message;
