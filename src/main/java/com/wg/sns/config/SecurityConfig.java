f97552d9c9a8b332be1ae500dc153a5904adf9d6
leewg97 <leewg97@naver.com>
2022-11-09 15:48:31 +0900

    Feat #2 : Add JwtSecurityConfig & Refactor SecurityConfig * SecurityConfig, JwtSecurityConfig와 분리

--- src/main/java/com/wg/sns/config/SecurityConfig.java
+++ src/main/java/com/wg/sns/config/SecurityConfig.java
@@ -1,33 +1,17 @@
 package com.wg.sns.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
-                .antMatchers("/api/**")
-                .authenticated();
-    }
 }
