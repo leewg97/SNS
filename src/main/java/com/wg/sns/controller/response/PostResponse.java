7f0691a84afdbb5ff9ef77d90111e4edaff82e1b
leewg97 <leewg97@naver.com>
2022-11-11 01:32:42 +0900

    Feat #2 : Add PostResponse
    * 포스트 수정 response dto 추가

---
+++ src/main/java/com/wg/sns/controller/response/PostResponse.java
@@ -0,0 +1,33 @@
+package com.wg.sns.controller.response;
+
+import com.wg.sns.model.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Getter
+@AllArgsConstructor
+public class PostResponse {
+
+    private Long id;
+    private String title;
+    private String body;
+    private UserResponse user;
+    private Timestamp registeredAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+    public static PostResponse fromPost(Post post) {
+        return new PostResponse(
+                post.getId(),
+                post.getTitle(),
+                post.getBody(),
+                UserResponse.fromUser(post.getUser()),
+                post.getRegisteredAt(),
+                post.getUpdatedAt(),
+                post.getDeletedAt()
+        );
+    }
+
+}
