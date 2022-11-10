fcba3731df7c060236a22477b316d619cd81dca4
leewg97 <leewg97@naver.com>
2022-11-11 01:34:03 +0900

    Test #2 : Update tests
    * 포스트 수정 관련 테스트 작성

--- src/test/java/com/wg/sns/controller/PostControllerTest.java
+++ src/test/java/com/wg/sns/controller/PostControllerTest.java
@@ -1,7 +1,12 @@
 package com.wg.sns.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wg.sns.controller.request.UserJoinRequest;
+import com.wg.sns.controller.request.PostCreateRequest;
+import com.wg.sns.controller.request.PostModifyRequest;
+import com.wg.sns.exception.ErrorCode;
+import com.wg.sns.exception.SnsApplicationException;
+import com.wg.sns.fixture.PostEntityFixture;
+import com.wg.sns.model.Post;
 import com.wg.sns.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +19,12 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,13 +42,13 @@ public class PostControllerTest {
     @WithMockUser
     @DisplayName("포스트 작성이 성공한 경우")
     @Test
-    void postCreationSuccessful() throws Exception {
+    void postCreationIsSuccessful() throws Exception {
         String title = "title";
         String body = "body";
 
         mockMvc.perform(post("/api/v1/posts")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest("title", "body"))))
+                        .content(objectMapper.writeValueAsBytes(new PostCreateRequest("title", "body"))))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
@@ -46,16 +56,78 @@ public class PostControllerTest {
     @WithAnonymousUser
     @DisplayName("포스트 작성 전 로그인 하지 않은 경우")
     @Test
-    void doNotLoginWhenCreatingPost() throws Exception {
+    void ifYouAreNotLoggedInBeforeCreatingAPost() throws Exception {
         String title = "title";
         String body = "body";
 
         mockMvc.perform(post("/api/v1/posts")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest("title", "body"))))
+                        .content(objectMapper.writeValueAsBytes(new PostCreateRequest("title", "body"))))
                 .andDo(print())
                 .andExpect(status().isUnauthorized());
     }
 
+    @WithMockUser
+    @DisplayName("포스트 수정이 성공한 경우")
+    @Test
+    void postModificationIsSuccessful() throws Exception {
+        String title = "title";
+        String body = "body";
+
+        when(postService.modify(eq(title), eq(body), any(), any()))
+                .thenReturn(Post.fromEntity(PostEntityFixture.get("username", 1L, 1L)));
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @WithAnonymousUser
+    @DisplayName("포스트 수정 전 로그인 하지 않은 경우")
+    @Test
+    void ifYouAreNotLoggedInBeforeModifyingThePost() throws Exception {
+        String title = "title";
+        String body = "body";
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithMockUser
+    @DisplayName("본인이 작성하지 않은 포스트를 수정하려는 경우 에러 발생")
+    @Test
+    void ifThePostIsNotWrittenByYouWhenModifyingIt() throws Exception {
+        String title = "title";
+        String body = "body";
+
+        doThrow(new SnsApplicationException(ErrorCode.INVALID_PERMISSION)).when(postService).modify(eq(title), eq(body), any(), eq(1L));
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithMockUser
+    @DisplayName("수정하려는 포스트 아이디가 존재하지 않는 경우 에러 발생")
+    @Test
+    void thePostIdYouWantToModifyDoesNotExist() throws Exception {
+        String title = "title";
+        String body = "body";
+
+        doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).modify(eq(title), eq(body), any(), eq(1L));
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
 
 }
