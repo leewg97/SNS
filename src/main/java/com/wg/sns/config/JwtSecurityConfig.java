db2fc2585c52893b3dd2d871210f5da464923baf
leewg97 <leewg97@naver.com>
2022-11-11 03:25:13 +0900

    Feat : Update JWT
    * config 설정 추가
    * error log 수정

--- src/main/java/com/wg/sns/config/JwtSecurityConfig.java
+++ src/main/java/com/wg/sns/config/JwtSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,6 +23,11 @@ public class JwtSecurityConfig {
 
     public JwtSecurityConfig(UserService userService) {
         this.userService = userService;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().regexMatchers("^(?!/api/).*");
     }
 
     @Bean
