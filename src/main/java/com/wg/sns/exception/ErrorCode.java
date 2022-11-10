1ade7e2c49ba96121916af4139fa6c5ed316b6bb
leewg97 <leewg97@naver.com>
2022-11-11 01:31:01 +0900

    Feat #2 : Update ErrorCode
    * Add INVALID_PERMISSION error code

--- src/main/java/com/wg/sns/exception/ErrorCode.java
+++ src/main/java/com/wg/sns/exception/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid"),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 
