9d0472a97d6c7a27929021e58d49ee039e081a72
leewg97 <leewg97@naver.com>
2022-11-11 01:32:56 +0900

    Feat #2 : Update PostService
    * 포스트 수정 로직 추가

--- src/main/java/com/wg/sns/service/PostService.java
+++ src/main/java/com/wg/sns/service/PostService.java
@@ -2,6 +2,7 @@ package com.wg.sns.service;
 
 import com.wg.sns.exception.ErrorCode;
 import com.wg.sns.exception.SnsApplicationException;
+import com.wg.sns.model.Post;
 import com.wg.sns.model.entity.PostEntity;
 import com.wg.sns.model.entity.UserEntity;
 import com.wg.sns.repository.PostEntityRepository;
@@ -23,6 +24,25 @@ public class PostService {
                 () -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username))
         );
         postEntityRepository.save(PostEntity.of(title, body, userEntity));
+    }
+
+    public Post modify(String title, String body, String username, Long postId) {
+        UserEntity userEntity = userEntityRepository.findByUsername(username).orElseThrow(
+                () -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username))
+        );
+
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(
+                () -> new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId))
+        );
+
+        if (postEntity.getUserEntity() != userEntity) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", username, postId));
+        }
+
+        postEntity.setTitle(title);
+        postEntity.setBody(body);
+
+        return Post.fromEntity(postEntityRepository.saveAndFlush(postEntity));
     }
 
 }
