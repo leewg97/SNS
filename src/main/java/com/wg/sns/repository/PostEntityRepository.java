b98a9d50a5d28e20f528535da9abebf6bfc01818
leewg97 <leewg97@naver.com>
2022-11-11 02:28:36 +0900

    Feat #2 : Update PostEntityRepository * 쿼리 메소드 추가

--- src/main/java/com/wg/sns/repository/PostEntityRepository.java
+++ src/main/java/com/wg/sns/repository/PostEntityRepository.java
@@ -1,7 +1,11 @@
 package com.wg.sns.repository;
 
 import com.wg.sns.model.entity.PostEntity;
+import com.wg.sns.model.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostEntityRepository extends JpaRepository<PostEntity, Long> {
+    Page<PostEntity> findAllByUserEntity(UserEntity userEntity, Pageable pageable);
 }
