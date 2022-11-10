9196293b34bb6bddb644b156df6f5f1efd8babc7
leewg97 <leewg97@naver.com>
2022-11-11 01:31:42 +0900

    Refactor #2 : Refactor Post

--- src/main/java/com/wg/sns/model/Post.java
+++ src/main/java/com/wg/sns/model/Post.java
@@ -10,7 +10,7 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 public class Post {
 
-    private Long id = null;
+    private Long id;
     private String title;
     private String body;
     private User user;
