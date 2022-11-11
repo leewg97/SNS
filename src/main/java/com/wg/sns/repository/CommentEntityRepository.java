e70f87f001a3cbf050d4d61080610a98411ebf88
leewg97 <leewg97@naver.com>
2022-11-11 23:44:29 +0900

    Feat : Update CommentEntityRepository * delete 쿼리 추가

--- src/main/java/com/wg/sns/repository/CommentEntityRepository.java
+++ src/main/java/com/wg/sns/repository/CommentEntityRepository.java
@@ -5,7 +5,17 @@ import com.wg.sns.model.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Long> {
     Page<CommentEntity> findAllByPostEntity(PostEntity postEntity, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("update CommentEntity entity set deleted_at = NOW() where entity.postEntity = :post")
+    void deleteAllByPost(@Param("post") PostEntity postEntity);
+
 }
